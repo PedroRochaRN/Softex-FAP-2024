0e8570aea8207af7fb37d36072862643fbd7994b
Pedro Vinícius <128431841+PedroRochaRN@users.noreply.github.com>
2024-08-26 19:24:46 -0300

    Delete Gerador de Intervalo - 03.07.24.py

--- Gerador de Intervalo - 03.07.24.py
+++
@@ -1,9 +0,0 @@
-def intervalo(inicio, fim, passo=1):
-    atual = inicio
-    while atual < fim:
-        yield atual
-        atual += passo
-
-# Exemplo de uso
-for numero in intervalo(1, 10, 2):
-    print(numero)
